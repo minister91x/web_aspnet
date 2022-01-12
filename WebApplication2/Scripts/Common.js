c7e968ddfa48fb96918871f06b5606dc5df1016b
Quân <quan91.nt@gmail.com>
2022-01-12 22:15:15 +0700

    Buoi 4

---
+++ WebApplication2/Scripts/Common.js
@@ -0,0 +1,18 @@
+﻿window.Common = {
+    ShowPoup: function (msgInput) {
+        var data = {
+            msg: msgInput
+        };
+
+        $.ajax({
+            type: "GET", // GET / POST / DELETE
+            cache: false,
+            url: "/Common/PopupPartial",
+            data: data,
+            success: function (data) {
+                $("#PopupDiv").html(data);
+                $("#myModalPopup").modal("show");;
+            }
+        });
+    }
+};

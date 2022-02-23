5036c8d2fb58c8b1c84eafd6adf833fe2296f2d7
Quân <quan91.nt@gmail.com>
2022-02-23 20:34:11 +0700

    ngay 21-2-2022

---
+++ DataAccess.Student/DTO/UserFunction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Student.DTO
+{
+    public class UserFunction
+    {
+        public int UserID { get; set; }
+        public int FunctionID { get; set; }
+        public int IsView { get; set; }
+        public int IsInsert { get; set; }
+        public int IsUpdate { get; set; }
+        public int IsDelete { get; set; }
+    }
+}
